18bed116567571f64ccc01c4fab9f90798aea0aa
NepEvg <eneputina@inbox.ru>
2022-01-26 10:26:21 +0500

    double project

---
+++ BranchProject/BranchProject/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("введите А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("какое действие выполнить (+, -, *, /) : ");
+string ans = Console.ReadLine();
+switch (ans)
+{
+    case "+":
+
+        break;
+    case "-":
+
+        break;
+    case "*":
+
+        break;
+    case "/":
+
+        break;
+}
